b6cd33a99e0e8ab72810058258b4c60432be094d
temiyores <temiyores1234@gmail.com>
2023-12-12 03:15:16 +0100

    sixth commit

--- src/components/TestimonialCarousel/Carousel.tsx
+++ src/components/TestimonialCarousel/Carousel.tsx
@@ -1,43 +1,43 @@
-import  { useEffect, useState } from "react";
+// import  { useEffect, useState } from "react";
 import "./carousel.scss";
-import { Img22, Img40, Img23 } from "../../assets/images";
+// import { Img22, Img40, Img23 } from "../../assets/images";
 const Testimonials = () => {
-  const [isMobile, setIsMobile] = useState(true);
+  // const [isMobile, setIsMobile] = useState(true);
 
-  const handleResize = () => {
-    if (window.innerWidth < 720) {
-      setIsMobile(true);
-    } else {
-      setIsMobile(false);
-    }
-  };
+  // const handleResize = () => {
+  //   if (window.innerWidth < 720) {
+  //     setIsMobile(true);
+  //   } else {
+  //     setIsMobile(false);
+  //   }
+  // };
   // create an event listener
-  useEffect(() => {
-    window.addEventListener("resize", handleResize);
-  });
-  const breakpoints = [
-    { width: 1, itemsToShow: 1 },
-    { width: 550, itemsToShow: 2 },
-    { width: 768, itemsToShow: 3 },
-    { width: 1024, itemsToShow: 3 },
-  ];
-  const testimonialData = [
-    {
-      name: "Mrs. A. Adeyemi",
-      image: Img40,
-      desc: "Big thanks to Novarick homes, I was able to land my dream land 🚀",
-    },
-    {
-      name: "Mrs. O. Ogologo",
-      image: Img22,
-      desc: "Big thanks to Novarick homes, I was able to land my dream land 🚀",
-    },
-    {
-      name: "Mrs. B. Boma",
-      image: Img23,
-      desc: "Big thanks to Novarick homes, I was able to land my dream land 🚀",
-    },
-  ];
+  // useEffect(() => {
+  //   window.addEventListener("resize", handleResize);
+  // });
+  // const breakpoints = [
+  //   { width: 1, itemsToShow: 1 },
+  //   { width: 550, itemsToShow: 2 },
+  //   { width: 768, itemsToShow: 3 },
+  //   { width: 1024, itemsToShow: 3 },
+  // ];
+  // const testimonialData = [
+  //   {
+  //     name: "Mrs. A. Adeyemi",
+  //     image: Img40,
+  //     desc: "Big thanks to Novarick homes, I was able to land my dream land 🚀",
+  //   },
+  //   {
+  //     name: "Mrs. O. Ogologo",
+  //     image: Img22,
+  //     desc: "Big thanks to Novarick homes, I was able to land my dream land 🚀",
+  //   },
+  //   {
+  //     name: "Mrs. B. Boma",
+  //     image: Img23,
+  //     desc: "Big thanks to Novarick homes, I was able to land my dream land 🚀",
+  //   },
+  // ];
   return (
     <>
       {/* <Carousel itemPadding={[0, 10]}
